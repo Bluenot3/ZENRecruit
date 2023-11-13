7ece93f3c516e119e7370cb36f75386e407e0bac
ZEN <89049892+Bluenot3@users.noreply.github.com>
2023-11-13 01:13:21 -0500

    Update search.py

--- search.py
+++ search.py
@@ -1,3 +1,10 @@
+import os
+
 def search_candidates(resume_criteria):
-    # Implement the search logic here
+    openai_api_key = os.getenv('OPENAI_API_KEY')
+    if not openai_api_key:
+        raise ValueError("OpenAI API key not found. Set the OPENAI_API_KEY environment variable.")
+
+    # Implement the search logic here, using the OpenAI API key as needed
+    
     return "Search results based on the criteria"
